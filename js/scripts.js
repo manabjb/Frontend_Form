76ab463725b0a331c384a937ccdb2c81e67eae18
ManabJB <55902693+manabjb@users.noreply.github.com>
2021-08-14 02:37:46 +0530

    JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 
+ var i= 0;
+    var txt="Hi Welcome To This Service";
+    var speed= 100;
+    function type(){
+    document.getElementById("type").innerHTML
+    +=txt.charAt(i);
+    i++;
+    setTimeout(type,speed);
+    }
+    type();
